bfb6ede9105cc868f98824404d484b0a9ca82cf6
Wawesh2020 <ghenry429@gmail.com>
2020-03-30 20:03:55 -0400

    Changes to validate Password to run efficiently

--- index.js
+++ index.js
@@ -1,61 +1,40 @@
-// eslint-disable-next-line no-unused-vars 
-function validatePassword(password) { 
 
-  function length(password) {
-    if(password.length < 8) {
-      return false
-    }
-    else {
-      return true 
-    }
-  }
-  function upperCase(password){ 
-    if (password.toUpperCase() != password) {
+function isUpperCase(password, index){
+  return password.charCodeAt(index) >= 65 && password.charCodeAt(index) <= 90
+}
   
-      return true
-    }
-    else {
-      return false
-    }
-  }
-  function LowerCase(password){
-    if (password.toLowerCase() != password) 
-    {
-      return true
-    }
-    else
-    {
-      return false
-    }
-  }
-
-  function character (password){
-    if (password.includes('!', '#', '$', '@')){
-      return true
-    }
-    else {
-      return false 
-    }
-    
-  }
-
-  function integer (password){
-    if (password.includes('0', '1', '2', '3', '4', '5', '6', '7', '8', '9')){
-      return true
-    }
-    else {
-      return false
-    }
-  }
-
-  if (integer(password) && character(password) && LowerCase(password) && upperCase(password) && length(password)){
-    return true
-  }
-  else {
-    return false 
-  }
-
+function isLowerCase(password, index){
+  return password.charCodeAt(index) >= 97 && password.charCodeAt(index) <= 122
 }
 
+function isNumeric(password, index){ 
+  return !isNaN(password[index])
+}
 
-module.exports = validatePassword 
+function validatePassword(password) {
+  if(password.length < 8) return false
+
+  let upper = 0
+  let lower = 0
+  let numeric = 0
+  let special = 0
+
+  for(let i = 0; i < password.length; i++){
+    if(isUpperCase(password, i)){
+      upper++ 
+    } else if(isLowerCase(password, i)) {
+      lower++
+    } else if (isNumeric(password, i)) {
+      numeric++
+    } else {
+      special++
+    }
+  }
+     
+  return upper > 0 && lower > 0 && numeric > 0 && special > 0 
+}
+ 
+
+
+module.exports =  validatePassword 
+ 
